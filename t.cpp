38dc262f7f675d59252a61d4a91c4c053f9f36e2
EdCanCe <edcance.dev@gmail.com>
2024-05-15 22:21:03 -0600

    Initial commit

---
+++ t.cpp
@@ -0,0 +1,26 @@
+/*Edmundo Canedo Cervantes - A01645576 */
+#include <bits/stdc++.h>
+#define fore(i, l, r) for (long long i = (l); i < (r); i++)
+#define forex(i, l, r) for (long long i = (l); i >= (r); i--)
+#define ll long long
+#define ull unsigned long long
+#define nl cout<<"\n"
+#define cnl "\n"
+#define rfc "\033[31;1m"
+#define gfc "\033[32;1m"
+#define yfc "\033[33;1m"
+#define bfc "\033[34;1m"
+#define pfc "\033[35;1m"
+#define cfc "\033[36;1m"
+#define nfc "\033[0m"
+#define pb push_back
+using namespace std;
+
+int main(){
+
+    FILE *fp=freopen("hola.html", "w", stdout);  //Elimina esta linea
+    cout<<"<p>holatastis</p><h1>hello</h1>";
+    fclose(fp);
+    system("hola.html");
+    cout<<"\n";
+}
